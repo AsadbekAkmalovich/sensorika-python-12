b7763ed0ccb1a8f8a35a6054d49a0779a854ecaa
AsadbekAkmalovich <127301362+AsadbekAkmalovich@users.noreply.github.com>
2023-03-11 16:46:49 +0500

    Update main.py

--- 03-dars/main.py
+++ 03-dars/main.py
@@ -1,1 +1,11 @@
+# Admin
+loginn = input("Loginingizni kiriting: ")
+paroll = input("Parolingizni kiriting: ")
 
+login = ("asadbekasadbek@gmail.com")
+parol = ("121210")
+
+if loginn  == login and paroll == parol:
+    print("Xush kelibsiz ADMIN")
+else:
+    print("Men sizni tanimadim")
